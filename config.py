642c0bc8644fe7b5944514d435dc94a53aa41988
Josh Thomas <personettelabs@gmail.com>
2012-06-03 13:39:20 -0400

    Heroku changes

--- config.py
+++ config.py
@@ -3,10 +3,12 @@
     This file holds constants that control how the website behaves. 
 """
 
+import os
+
 # The Host of CatGuice server. 
 HOST = "0.0.0.0"
 # The Port the server will run off of
-PORT = 80
+PORT = int(os.environ.get('PORT', 5000))
 
 # If this is set to True, then the server will automatically reload modules that have been changed. 
 # Set this to False when you're running the website in production mode. 
