e86c78822b31572c6d276afdcdd4b766fc617fa3
Josh Thomas <personettelabs@gmail.com>
2012-06-03 13:27:10 -0400

    Minor fix

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 
 def main():
-  app.run(host=config.HOST, port=config.PORT, debug=config.DEVELOPMENT)
+  app.run()
 
 @app.route("/")
 def hello():
