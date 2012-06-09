a4bb88e88f71d3fd30725a8e53e9be925fa45d18
Josh Thomas <personettelabs@gmail.com>
2012-06-09 16:16:09 -0400

    Adds an about page, cleans up the navigation, and adds the beginnings to a product display widget.

--- server.py
+++ server.py
@@ -9,8 +9,12 @@ def main():
   app.run(host=config.HOST,port=config.PORT,debug=config.DEVELOPMENT)
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.errorhandler(404)
 def unauthorized(e):
