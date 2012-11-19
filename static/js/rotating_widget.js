f9e04de862d29283a787d4383366b2eb291f9677
Josh Thomas <personettelabs@gmail.com>
2012-11-18 21:47:21 -0500

    Adds the rotating widget to the top of the front page.

---
+++ static/js/rotating_widget.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	var next = function() {
+		$(".rotating_widget ul li:first-child").appendTo(".rotating_widget ul").slideUp();
+		$(".rotating_widget .next_button").unbind("click");
+		$(".rotating_widget ul li:first-child").slideDown(function () {
+			$(".rotating_widget .next_button").click(next);
+		});
+	}
+	setInterval(next, 4000);
+
+	$(".rotating_widget ul li:first-child").show();
+	$(".rotating_widget .next_button").click(next);
+});
