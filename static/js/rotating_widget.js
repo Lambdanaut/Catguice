3aeff4c9869faa83689e8bf4ed35b1ccbb068427
Josh Thomas <personettelabs@gmail.com>
2012-11-19 22:11:04 -0500

    Stops the front page widget from rotating through images when the mouse is hovering over it.

--- static/js/rotating_widget.js
+++ static/js/rotating_widget.js
@@ -1,13 +1,22 @@
 $(document).ready(function () {
-	var next = function() {
-		$(".rotating_widget ul li:first-child").appendTo(".rotating_widget ul").slideUp();
-		$(".rotating_widget .next_button").unbind("click");
-		$(".rotating_widget ul li:first-child").slideDown(function () {
-			$(".rotating_widget .next_button").click(next);
-		});
+	var rotate_on = true;
+	var next = function(clicked) {
+		if (rotate_on || clicked) {
+			$(".rotating_widget ul li:first-child").appendTo(".rotating_widget ul").slideUp();
+			$(".rotating_widget .next_button").unbind("click");
+			$(".rotating_widget ul li:first-child").slideDown(function () {
+				$(".rotating_widget .next_button").click(next);
+			});
+		}
 	}
-	setInterval(next, 5000);
+	setInterval(function () {next(false)}, 5000);
 
 	$(".rotating_widget ul li:first-child").show();
-	$(".rotating_widget .next_button").click(next);
+	$(".rotating_widget .next_button").click(function () {next(true)});
+	$(".rotating_widget").mouseover(function () {
+		rotate_on = false;
+	});
+	$(".rotating_widget").mouseout(function () {
+		rotate_on = true;
+	});
 });
